a21897fd6793bfab1369232bed621515a3247de7
tianxiang1989 <qimingbuyi@sina.com>
2014-07-01 18:04:14 +0800

    111 some modify

--- src/fox/test/shownew/WhatsnewDoor.java
+++ src/fox/test/shownew/WhatsnewDoor.java
@@ -29,7 +29,7 @@ public class WhatsnewDoor extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login);
+		setContentView(R.layout.login_show_new);
 
 		mUp = (ImageView) findViewById(R.id.imageUp);
 		mDown = (ImageView) findViewById(R.id.imageDown);
